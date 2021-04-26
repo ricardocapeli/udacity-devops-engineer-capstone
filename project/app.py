eb137049b76e2e8a891958d2e50fc09ac9897720
Ricardo Capeli <ricardo.capeli@ibm.com>
2021-04-25 21:52:04 -0300

    error lint

--- project/app.py
+++ project/app.py
@@ -3,11 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask: Hello World from Docker'
+    return 'Hello Wolrd, my name is Ricardo DevOps Engineer this my Capstone Projet. Acess /api.'
 
 @app.route('/api')
 def rest_hello_world():
-    return '{"id":1,"message":"Hello Wolrd Teste"}'
+    return '{"id":1,"message":"Hello Wolrd Teste my API project Ricardo API DevOps Engineer"}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
+
+    rearefasdasdf erro lint
